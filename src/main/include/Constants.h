5e3ae436f81496e87a3fa68634c8c6640f311a04
Ken Ocheltree <kbocheltree@gmail.com>
2022-11-15 00:52:35 -0500

    Read gyro 1x per, Dash to end,  Fix float consts

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -12,16 +12,13 @@
 #define DriveEncoderVelocityFactor (DriveEncoderPosFactor / 60)
 #define turnEncoderPosFactor (TurningMotorGearRatio * M_PI *2)
 #define turnEncoderVelocityFactor (turnEncoderPosFactor / 60)
-
-#define MAXMotorSPEED 4.4196 //in meters per second converted from ft/s
-#define MAXTotalSPEED 4.4196/4 
-#define MAXOmega 1//Calculate after taking dimensions
-
+#define MAXMotorSPEED 4.4196 //in meters per sec 
+#define MAXOmega 1.0 //Calculate after taking dimensions
 //PID for Drive
-#define KDp 0
-#define KDi 0
-#define KDd 0
+#define KDp 1.0
+#define KDi 0.0
+#define KDd 0.0
 //PID for Rotate
-#define KRp 1
+#define KRp 1.0
 #define KRi 0.0
 #define KRd 0.0
