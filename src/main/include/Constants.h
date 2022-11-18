535a2a9008c0bb3a7fcf77963bfe4c64e0ea480b
Ken Ocheltree <kbocheltree@gmail.com>
2022-11-18 16:32:46 -0500

    Revert "Merge pull request #2 from Ossining-Engineering-Club/clean"
    This reverts commit 8e4240f8fd7261e61f6ddb2d1ea1a6db2607e43e, reversing
    changes made to 236fe903ec883514ea993e0a653541dbf5a6cb92.

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -1,6 +1,9 @@
 #pragma once
-
-#define FIELD_ORIENTED true
+//offsets
+#define RFZero 4.519107
+#define RBZero 3.092505
+#define LFZero -0.766990
+#define LBZero -0.510816
 //Gear and module related stuff
 #define TurningMotorGearRatio 7.0/150.0 //find out
 #define DrivingMotorGearRatio 1/6.75 //find out
@@ -9,28 +12,16 @@
 #define DriveEncoderVelocityFactor (DriveEncoderPosFactor / 60)
 #define turnEncoderPosFactor (TurningMotorGearRatio * M_PI *2)
 #define turnEncoderVelocityFactor (turnEncoderPosFactor / 60)
-#define MAX_SPEED 4.441 //Maximum wheel speed in meters per sec 
-#define MAX_TURN_RATE 4.0 //In Radians per second
 
-//Offsets for Absolute Encoders
-#define RFZERO (1.761010-1.57079632)
-#define RBZERO (3.204486-1.57079632)
-#define LFZERO (5.514661-1.57079632)
-#define LBZERO (5.787710-1.57079632)
-#define MODULE_MAX_ANGULAR_VELOCITY (M_PI *1_rad_per_s)
-#define MODULE_MAX_ANGULAR_ACCELERATION (M_PI*2_rad_per_s / 1_s)
-//SwerveModule PID for Drive
-#define KDP 1.0
-#define KDI 0.0
-#define KDD 0.0
-//SweverModule PID for Rotate
-#define KRP 1.0
-#define KRI 0.0
-#define KRD 0.0
-//Drivetrain:
-// Define values for GetValue Switch Statement
-#define L_FRONT 0
-#define R_FRONT 1
-#define L_BACK 2
-#define R_BACK 3
-#define ABS_ANGLE 0
+#define MAXMotorSPEED 4.4196 //in meters per second converted from ft/s
+#define MAXTotalSPEED 4.4196/4 
+#define MAXOmega 1//Calculate after taking dimensions
+
+//PID for Drive
+#define KDp 0
+#define KDi 0
+#define KDd 0
+//PID for Rotate
+#define KRp 1
+#define KRi 0.0
+#define KRd 0.0
