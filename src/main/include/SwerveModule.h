ee0e6c5675ee4b2278f05c02084ffbff94e1a931
Ken Ocheltree <kbocheltree@gmail.com>
2022-11-18 18:32:34 -0500

    Clean up afeter merge issues from Clean

--- src/main/include/SwerveModule.h
+++ src/main/include/SwerveModule.h
@@ -1,59 +1,50 @@
 #pragma once
-#include <frc/smartdashboard/SmartDashboard.h>
 #include <frc/motorcontrol/Spark.h>
 #include <frc/CounterBase.h>
-
 #include <frc/kinematics/SwerveDriveOdometry.h>
 #include <frc/kinematics/SwerveDriveKinematics.h>
-#include <frc/geometry/Rotation2d.h>
-#include <frc/geometry/Pose2d.h>
-#include <cmath>
 #include <frc/controller/ProfiledPIDController.h>
+#include <frc/controller/PIDController.h>
+#include <frc/controller/SimpleMotorFeedforward.h>
 #include <units/angle.h>
-#include <frc/geometry/Translation2d.h>
 #include <units/base.h>
 #include <units/length.h>
-#include <frc/kinematics/ChassisSpeeds.h>
 #include <units/velocity.h>
-#include <units/voltage.h>
-#include <frc/controller/SimpleMotorFeedforward.h>
 #include <units/angular_velocity.h>
 #include "Constants.h"
-//Motors and Encoders
 #include "rev/CANSparkMax.h"
 #include <frc/Encoder.h>
 #include <Rev/CANEncoder.h>
-
-#include <frc/AnalogPotentiometer.h>
-#include <frc/AnalogInput.h>
-#include <frc/controller/PIDController.h>
-#include <bits/stdc++.h>
 #include <ctre/phoenix/sensors/CANCoder.h>
 
-using namespace frc;
 class SwerveModule{
-private:
+  public:
+	SwerveModule(int RotatorMotorNo, 
+		int DriveMotorNo, int CANCoderId, bool reverseDirection, 
+		double Offset, bool DriveReverse, bool TurnReverse);
+	frc::SwerveModuleState GetState() const;	
+	double GetDrivePower();
+	double GetRotatorPower();
+	void ResetEncoder();
+	double GetCurrentAngle();
+	double GetAbsEncoderAngle();
+	void SetToVector(frc::SwerveModuleState& state);
+
+  private:
 	rev::CANSparkMax RotatorMotor;
 	rev::CANSparkMax DriveMotor;
 	ctre::phoenix::sensors::CANCoder absEncoder;
 	rev::SparkMaxRelativeEncoder * driveEncoder;
 	rev::SparkMaxRelativeEncoder * turningEncoder;
-	  SmartDashboard* dash;
-	
-	PIDController turningPidController;
-	
-	
-public:
-	SwerveModule(int RotatorMotorNo, int DriveMotorNo, int CANCoderId, bool reverseDirection, double Offset, bool DriveReverse, bool TurnReverse);
-	
-	//abs encoder vals
-	double absSignum;
-	double encoderOffset;
-	double GetDrivePower(frc::SwerveModuleState& state);
-	double GetRotatorPower(frc::SwerveModuleState& state);
-	void ResetEncoder();
-	double GetCurrentPosition();
-	double GetAbsEncoderPosition();
-	void SetToVector(frc::SwerveModuleState& state);
-	
+	double absSignum; 		//abs encoder vals
+	double absEncoderOffset;  //Set on init from constants
+	double turningEncoderOffset;
+	frc2::PIDController drivePIDController{KDP, KDI, KDD};
+  	frc2::PIDController//<units::radians>
+	  turningPIDController{KRP, KRI, KRD};//,
+	  //{MODULE_MAX_ANGULAR_VELOCITY, MODULE_MAX_ANGULAR_ACCELERATION}};
+	frc::SimpleMotorFeedforward<units::meters>
+	 	drivingFeedforward{1_V, 3_V / 1_mps};
+  	frc::SimpleMotorFeedforward<units::radians> 
+	  	turningFeedforward{1_V, 0.5_V / 1_rad_per_s};
 };
